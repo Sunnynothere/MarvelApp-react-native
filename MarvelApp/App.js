436def0fd4eb20e04b4b41b24ede1f16577e9082
Sol Lizarraga Grigoni <grigonisol@hotmail.com>
2023-11-16 21:33:15 -0300

    fixing

--- MarvelApp/App.js
+++ MarvelApp/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Landing from './components/Landing';
+
+const Stack= createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Marvel App' component={Landing} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
---
+++ MarvelApp/components/Landing.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Image } from 'react-native';
+import Marvel from '../assets/marvel.webp';
+
+const Landing = () => {
+    return(
+        <View>
+            <Image  source={Marvel} />
+        </View>
+    )
+};
+
+export default Landing;
